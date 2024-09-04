919ab50ded7c0ca7a24aa8d8cdd252bce3c8c35b
beakerandjake <beakerandjake@users.noreply.github.com>
2024-09-04 13:58:11 -0700

    clean up

--- bin/deploy-resource.sh
+++ bin/deploy-resource.sh
@@ -2,6 +2,7 @@
 # Script: deploy-resource.sh
 # Description: Deploys a couldformation template 
 # Usage: ./deploy-resource.sh <file-name> <stack-name>
+set -e
 
 file_name=$1
 stack_name=$2
@@ -14,7 +15,7 @@ fi
 
 # ensure requested template file exists
 if [ ! -f $file_name ]; then
-    echo "could not find file at: $file_name"
+    echo "Could not find file at: $file_name"
     exit 1
 fi
 
@@ -22,5 +23,5 @@ fi
 file_arg="file://$file_name"
 
 # validates before deploy
-aws cloudformation validate-template --template-body $file_arg > /dev/null \
-    && aws cloudformation create-stack --stack-name $stack_name --template-body $file_arg
+aws cloudformation validate-template --template-body "$file_arg" > /dev/null
+aws cloudformation create-stack --stack-name "$stack_name" --template-body "$file_arg"
