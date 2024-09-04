e570f1cebfc17128cb26b61a030d0a80cdf6162e
beakerandjake <beakerandjake@users.noreply.github.com>
2024-09-04 11:29:04 -0700

    add deploy cf resource helper script

---
+++ bin/deploy-resource.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ensure correct argument usage
+if [ "$#" -ne 2 ]; then
+    echo "usage: $0 <file-name> <stack-name>"
+    exit 1
+fi
+
+# ensure requested template file exists
+if [ ! -f $1 ]; then
+    echo "could not find file at: $1"
+    exit 1
+fi
+
+# aws cli expects local templates to be prefixed with file://
+file_arg="file://$1"
+
+# validates the specified cloudformation template then deploys to aws
+aws cloudformation validate-template --template-body $file_arg > /dev/null &&
+aws cloudformation create-stack --stack-name $2 --template-body $file_arg
