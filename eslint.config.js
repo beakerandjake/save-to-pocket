96f0a94aacfda6f1be65b29cb772933fd36e99b2
beakerandjake <beakerandjake@users.noreply.github.com>
2024-09-05 08:43:14 -0700

    add custom eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -6,4 +6,22 @@ export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
+  {
+    rules: {
+      'no-duplicate-imports': 'error',
+      'no-template-curly-in-string': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+      'consistent-return': 'error',
+      curly: 'error',
+      'no-var': 'error',
+      'default-param-last': 'error',
+      'func-style': 'error',
+      'guard-for-in': 'error',
+      'no-else-return': 'error',
+      'no-lonely-if': 'error',
+      'prefer-arrow-callback': 'error',
+      'prefer-const': 'error',
+      'prefer-template': 'error',
+    },
+  },
 ];
