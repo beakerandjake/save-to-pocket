9521ab5e57bccdbd2af219d05735476819014c3a
beakerandjake <beakerandjake@users.noreply.github.com>
2024-09-05 08:09:18 -0700

    wip adding env vars

--- src/save-items/index.js
+++ src/save-items/index.js
@@ -1,4 +1,10 @@
 export const handler = async (event) => {
+  console.log(
+    `access key param name: ${process.env.POCKET_ACCESS_KEY_PARAM_NAME}`,
+  );
+  console.log(
+    `consumer key param name: ${process.env.POCKET_CONSUMER_KEY_PARAM_NAME}`,
+  );
   console.log(`hello: ${JSON.stringify(event)}`);
   return {
     statusCode: 200,
