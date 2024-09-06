734de17af16ba25ce58974264786ded10f364697
beakerandjake <beakerandjake@users.noreply.github.com>
2024-09-06 05:40:46 -0700

    add workspaces and install sdk in devDependencies for intellisense

---
+++ src/authorize/users.js
@@ -0,0 +1,9 @@
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({
+  
+})
+
+export const getHashedPassword = async (userId) => {
+  
+};
